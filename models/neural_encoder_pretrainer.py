d6bfdd88263e193bf97209365bb93335f06a9903
Cole Hurwitz <colehurwitz@gmail.com>
2023-11-10 13:56:18 -0500

    pretraining first draft

--- models/neural_encoder_pretrainer.py
+++ models/neural_encoder_pretrainer.py
@@ -15,17 +15,21 @@ class NeuralEncoderPretrainer(nn.Module):
 
         self.encoder = encoder
         self.rate_dropout = nn.Dropout(config.rate_dropout)
+        n_channels = encoder.n_channels
 
         # Build decoder
         decoder_layers = []
-        decoder_layers.append(nn.Linear(self.encoder.hidden_size, config.inter_dim))
-        decoder_layers.append(nn.ReLU())
-        decoder_layers.append(nn.Linear(config.inter_dim, num_neurons))
+        if config.multi_layer_decoder == False:
+            decoder_layers.append(nn.Linear(self.encoder.hidden_size, n_channels))
+        else:
+            decoder_layers.append(nn.Linear(self.encoder.hidden_size, config.inter_dim))
+            decoder_layers.append(nn.ReLU())
+            decoder_layers.append(nn.Linear(config.inter_dim, n_channels))
         if not config.use_lograte:
-            decoder_layers.append(nn.ReLU()) # If we're not using lograte, we need to feed positive ratesw
+            decoder_layers.append(nn.ReLU()) # If we're not using lograte, we need to feed positive rates
         
         self.decoder = nn.Sequential(*decoder_layers)
-        self.classifier = nn.PoissonNLLLoss(reduction='none', log_input=config.use_lograte)
+        self.loss = nn.PoissonNLLLoss(reduction='none', log_input=config.use_lograte)
 
     def forward(
             self, 
@@ -35,4 +39,21 @@ class NeuralEncoderPretrainer(nn.Module):
             block_idx:          torch.LongTensor,   # (batch_size, fea_len)
             date_idx:           torch.LongTensor,   # (batch_size, fea_len)
         ) -> torch.FloatTensor:                     # (1,)
+
+        # encode neural data
+        x = self.encoder(features, features_mask, features_timestamp, block_idx, date_idx)
+
+        # dropout for regularization
+        x = self.rate_dropout(x)
+
+        # transform neural embeddings into rates
+        log_rates = self.decoder(x)
+
+        # compute the loss over the unmasked outputs
+        masked_loss = self.loss(log_rates, features) * features_mask
+
+        # sum the masked loss
+        loss = masked_loss.sum()
+
+        return (log_rates, loss)
         
